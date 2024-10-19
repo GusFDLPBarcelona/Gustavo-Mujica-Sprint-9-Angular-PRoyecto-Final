f38c4a56e3c73f282dcbecc85ec8e8df1b9b9762
Gus <151650535+GusFDLPBarcelona@users.noreply.github.com>
2024-10-19 20:27:59 +0200

    Creación de la estructura e instalación de dependencias. Commit inicial.

---
+++ nndvvs/src/app/componentes/haupstadt/haupstadt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-haupstadt',
+  standalone: true,
+  imports: [],
+  templateUrl: './haupstadt.component.html',
+  styleUrl: './haupstadt.component.css'
+})
+export class HaupstadtComponent {
+
+}
